853a78e835447141f6b6699b9ece17de465e388d
lukecrabtree <luke@Administrators-MacBook-Pro-2.local>
2022-08-27 17:57:41 -0400

    code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,56 +1,119 @@
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const samples = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-const dataSet = d3.json(url).then(function(data) {
-    console.log(data);
-  });
+d3.json(samples).then((data) => {
 
+    var names = data.names;
+    d3.select('#selDataset').selectAll('option').data(names).enter().append('option').text((data) => {
+        return data;
+    });
+});
 
-  function init() {
+function optionChanged(value) {
+    d3.json(samples).then((data) => {
+        var samp_values = data.samples;
+        var otuIDs = [];
+        var sampleValues = [];
+        var otuLabels = [];
+        var otuArray = [];
 
-  let trace1 = {
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16],
-    type: "bar"
-  };
+        samp_values.forEach(person => {
+            if(person.id == value) {
+                otuIDs = person.otu_ids;
+                sampleValues = person.sample_values;
+                otuLabels = person.otu_labels;
+                otuIDs.map(otuSelection => {
+                    otuArray.push(`OTU: ${otuSelection}`);
+                });
+            }
+        });
+    
+        var washes = 0;
+        var metadata = data.metadata;
+        metadata.forEach(person => {
+            if(person.id == value){
+                var details = Object.entries(person);
+                washes = details[6][1];
+                d3.select('#sample-metadata').selectAll('p').data(details).enter().append('p').text(info => {
+                    return `${info[0]}: ${info[1]}`;
+                });
+            }
+        });
 
-  Plotly.newPlot("bar", [trace1]);
+        let top_ten = sampleValues.slice(0,10).reverse()
+        let top_ten_ids = otuArray.slice(0,10).reverse()
+        let bar_text = otuLabels.slice(0,10).reverse()
 
+        var bar_trace = {
+            x: top_ten,
+            y: top_ten_ids,
+            type: 'bar', 
+            orientation: 'h',
+            text: bar_text
+        }
+
+        var bar_data = [bar_trace];
+
+        var bar_layout = {
+            title: "Top Ten OTUs Present for Test Subject", 
+            height: 500,
+            width: 550
+        }
+
+        Plotly.newPlot('bar', bar_data, bar_layout);
+        
+        var bubble_trace = {
+            x: otuIDs,
+            y: sampleValues,
+            mode: 'markers',
+            marker: {
+                size: sampleValues,
+                color:otuIDs,
+                colorscale: 'YlGnBu'
+            },
+            text: otuLabels
+        }
+
+        var bubble_data = [bubble_trace];
+
+        var bubble_layout = {
+            title: "All OTUs Present for Test Subject", 
+            xaxis: {
+                title: "OTU ID"
+            }
+        }
+
+        Plotly.newPlot('bubble', bubble_data, bubble_layout);
+
+        var gauge_trace = [
+            {
+              domain: { x: [0, 1], y: [0, 1] },
+              value: washes,
+              title: "Test Subject: Belly Button Washing Frequency",
+              type: "indicator",
+              mode: "gauge+number+delta",
+              delta: {reference: 9, 'increasing': {color: "red"}},
+              gauge: {
+                axis: { range: [null, 9] },
+                bar: {color: "e6c9f5"}, 
+                    steps: [
+                        { range: [0,1], color: "f5f6f4" },
+                        { range: [1,2], color: "ecf5e4" },
+                        { range: [2,3], color: "d4f2b8" },
+                        { range: [3,4], color: "c0f291" },
+                        { range: [4,5], color: "a7e36f" },
+                        { range: [5,6], color: "83c744" },
+                        { range: [6,7], color: "4cab2c" },
+                        { range: [7,8], color: "27b83f" },
+                        { range: [8,9], color: "108f3c" },
+                        ]},
+            }
+          ];
+
+          var gauge_layout = { 
+              width: 600, height: 500, margin: { t: 20, b: 40 } 
+            };
+
+          Plotly.newPlot('gauge', gauge_trace, gauge_layout);
+    
+    });
 }
-
-//   let trace2 = {
-//     x: [0,1,2,3],
-//     y: [12,43,34,76],
-//     type: "bubble"
-//   };
-
-//   Plotly.newPlot("bubble", [trace2]);
-
-d3.selectAll("#selDataset").on("change", updatePlotly);
-
-// This function is called when a dropdown menu item is selected
-function updatePlotly() {
-  // Use D3 to select the dropdown menu
-  let dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  let dataset = dropdownMenu.property("value");
-
-  // Initialize x and y arrays
-  let x = [];
-  let y = [];
-
-  if (dataset === 'dataset1') {
-    x = [1, 2, 3, 4, 5];
-    y = [1, 2, 4, 8, 16];
-  }
-
-  else if (dataset === 'dataset2') {
-    x = [10, 20, 30, 40, 50];
-    y = [1, 10, 100, 1000, 10000];
-  }
-
-  // Note the extra brackets around 'x' and 'y'
-  Plotly.restyle("bar", "x", [x]);
-  Plotly.restyle("bar", "y", [y]);
-}
-
-init();
